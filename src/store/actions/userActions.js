b58d3202957c417c9bda8760dd37c077a583e0c6
lukaszinglot <luk.inglot@gmail.com>
2019-11-19 22:30:55 +0100

    Add fetchLogin and fetchRegister action to userActions in redux

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,5 +1,5 @@
-import { FETCH_USER } from "./actiontypes";
-import { Dispatch } from "redux";
+import heroku from "../../api/heroku";
+import { FETCH_USER, USER_LOGIN, USER_REGISTER } from "./actiontypes";
 
 export function fetchUsers(data) {
   return {
@@ -7,3 +7,30 @@ export function fetchUsers(data) {
     data
   };
 }
+
+export function fetchLogin(data) {
+  return {
+    type: USER_LOGIN,
+    data
+  };
+}
+
+export function fetchRegister(data) {
+  return {
+    type: USER_REGISTER,
+    data
+  };
+}
+
+export const userValidationLogin = (email, password) => dispatch => {
+  console.log(email);
+  heroku.post("/login", { email, password }).then(data => {
+    console.log(data);
+  });
+};
+
+export const userValidationRegister = (email, password) => dispatch => {
+  heroku.post("/register", { email, password }).then(data => {
+    console.log(data);
+  });
+};
