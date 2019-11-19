59e63202b4f561927db5d252df8446a1c00a0f6b
Jakub Sznajder <donatelek92@gmail.com>
2019-11-19 20:20:28 +0100

    Add auth components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Login from './Login/Login'
+import Register from './Register/Register'
 
 const App = () => {
   return (
     <div>
       <p>Projekt</p>
+      <Login />
+      <Register />
     </div>
   );
 };
