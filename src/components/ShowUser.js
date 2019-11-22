9089eaa8ae3cb6a1d18277c0e0b6df51e4b5b7d8
lukaszinglot <luk.inglot@gmail.com>
2019-11-22 01:37:10 +0100

    Add ShowUser component, to show user after Successful login

---
+++ src/components/ShowUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ShowUser extends Component {
+  render() {
+    return (
+      <div>
+        <p>Tutaj</p>
+      </div>
+    );
+  }
+}
+
+export default ShowUser;
