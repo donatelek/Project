05176ab98c955dd76bb05361f6cba3857611b404
lukaszinglot <luk.inglot@gmail.com>
2019-11-23 03:07:04 +0100

    Add new variables to main index.js file with firebase auth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./components/App";
+import App, { history } from "./components/App";
 import store from "./store/index.js";
+import { firebase } from "./firebase/firebase";
 
-ReactDOM.render(
+const jsx = (
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
+
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("root"));
+    hasRendered = true;
+  }
+};
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log("log in");
+    renderApp();
+    if (history.location.pathname === "/") {
+      history.push("/user");
+    }
+  } else {
+    renderApp();
+    history.push("/");
+  }
+});
