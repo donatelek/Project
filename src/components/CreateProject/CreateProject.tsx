79f269c8280b0fdb7280f24c4b8247317839dcea
Jakub Sznajder <donatelek92@gmail.com>
2019-11-27 23:01:35 +0100

    Connect createproject to db

--- src/components/CreateProject/CreateProject.tsx
+++ src/components/CreateProject/CreateProject.tsx
@@ -1,32 +1,116 @@
 import React, { Component } from 'react';
+import { db } from '../../firebase/firebase'
 
-class CreateProject extends Component {
-    state = {}
+interface CreateProjectState {
+    projectName: string,
+    // do zmiany na tablice
+    programmingLang: string,
+    description: string,
+    shortDescription: string,
+    mainGoals: string,
+    contact: string,
+}
+
+class CreateProject extends Component<CreateProjectState> {
+    state = {
+        projectName: '',
+        programmingLang: '',
+        description: '',
+        shortDescription: '',
+        mainGoals: '',
+        errors: {
+            projectName: false,
+            programmingLang: false,
+            description: false,
+            shortDescription: false,
+            mainGoals: false,
+        }
+    }
+
+    errorMessages = {
+        projectName: 'required/max 6 words',
+        programmingLang: 'choose at least 3 ',
+        description: 'description should be at least 12 words',
+        shortDescription: 'max 10-12 words,required',
+        mainGoals: 'required ? ?',
+    }
+
+    //any do zmiany
+    handleInputChange = (e: any): void => {
+        const name = e.target.name
+        const value = e.target.value
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onSubmit = (): void => {
+        const { projectName, programmingLang, description, shortDescription, mainGoals } = this.state;
+        // connection to firebase db
+        // db.collection('projects').get().then(data => data.docs.forEach(doc => {
+        //     console.log(doc.data())
+        // }))
+        const added = new Date()
+        db.collection('projects').add({
+            projectName,
+            shortDescription,
+            description,
+            programmingLang,
+            mainGoals,
+            added
+        })
+    }
+
     render() {
         return (
             <>
                 <h1>Project info</h1>
-                <label htmlFor="">Project name</label>
                 <br />
-                <input type="text" />
+                <label htmlFor="">Photo/logo</label>
                 <br />
-                <label htmlFor="">Programming languages</label>
+                <input type="file" name="photo" id="" onChange={this.handleInputChange} />
                 <br />
-                <input type="text" />
+                <div>Your logo</div>
                 <br />
-                <label htmlFor="">Description</label>
+                <label htmlFor="">Project name*</label>
                 <br />
-                <textarea name="" id="" cols={30} rows={10}></textarea>
+                <input name='projectName' type="text" onChange={this.handleInputChange} value={this.state.projectName} />
+                <br />
+                <div>{this.errorMessages.projectName}</div>
+                <br />
+                <label htmlFor="">Programming languages*</label>
+                <br />
+                <select name="programmingLang" id="" onChange={this.handleInputChange}>
+                    <option value="Javascript">Javascript</option>
+                    <option value="Javascripttt">Javascripttt</option>
+                    <option value="Javascript">Javascript</option>
+                    <option value="Javascript">Javascript</option>
+                    <option value="Javascript">Javascript</option>
+                    <option value="Javascript">Javascript</option>
+                    <option value="Javascript">Javascript</option>
+                </select>
+                <br />
+                <div>{this.errorMessages.programmingLang}</div>
+                <br />
+                <label htmlFor="">Description*</label>
+                <br />
+                <textarea name="description" id="" cols={30} rows={10} onChange={this.handleInputChange} value={this.state.description} ></textarea>
+                <br />
+                <div>{this.errorMessages.description}</div>
                 <br />
                 <label htmlFor="">Main Goals</label>
                 <br />
-                <textarea name="" id="" cols={30} rows={10}></textarea>
+                <textarea name="mainGoals" id="" cols={30} rows={10} onChange={this.handleInputChange} value={this.state.mainGoals} ></textarea>
                 <br />
-                <label htmlFor="">Contact</label>
+                <div>{this.errorMessages.mainGoals}</div>
                 <br />
-                <input type="text" />
+                <label htmlFor="">Short Description*</label>
                 <br />
-                <button>Save</button>
+                <input name='shortDescription' type="text" onChange={this.handleInputChange} value={this.state.shortDescription} />
+                <br />
+                <div>{this.errorMessages.shortDescription}</div>
+                <br />
+                <button onClick={this.onSubmit} >Save</button>
             </>
         );
     }
